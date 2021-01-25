7db6d2bd8c343032e44df02626112e3d8cf33003
jhed <gallegosmatiasjoel@gmail.com>
2021-01-25 20:55:05 -0300

    Creating Row.js structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Astrovideo clone frontend</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
+
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Row({ title }) {
+    return (
+        <div>
+            <h2>{title}</h2>
+            {/* container -> posters */}
+
+        </div>
+    )
+}
+
+export default Row;
