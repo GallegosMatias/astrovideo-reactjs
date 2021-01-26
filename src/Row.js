e39798ec453efd9f4fe2550034e7ca997e0d5ae8
jhed <gallegosmatiasjoel@gmail.com>
2021-01-26 11:59:46 -0300

    Fetch settings

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+   const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        
+        async function fecthData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+        }
+        fecthData();
+    }, []);
+
     return (
         <div>
             <h2>{title}</h2>
