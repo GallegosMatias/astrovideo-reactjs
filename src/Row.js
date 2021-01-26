571ee7aaac44e3ec651d23f681096c40536578b6
jhed <gallegosmatiasjoel@gmail.com>
2021-01-26 12:55:45 -0300

    Row Posters Styles

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -27,7 +28,11 @@ function Row({ title, fetchUrl }) {
                 {/*several row_posters*/}
 
                 {movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                    <img 
+                        key={movie.id}
+                        className="row_poster" 
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name}/>
                 ))}
             </div>
 
