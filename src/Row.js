bbb8a2430252a17e51ad04dc15294263444a66c3
jhed <gallegosmatiasjoel@gmail.com>
2021-01-26 12:15:42 -0300

    First map of movies

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl }) {
    const [movies, setMovies] = useState([]);
 
@@ -8,17 +10,26 @@ function Row({ title, fetchUrl }) {
         
         async function fecthData() {
             const request = await axios.get(fetchUrl);
-            console.log(request.data.results);
+            /*console.log(request.data.results); //See whats going on with the fetch*/
             setMovies(request.data.results);
             return request;
         }
         fecthData();
-    }, []);
+    }, [fetchUrl]);
+
+    console.table(movies);
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
-            {/* container -> posters */}
+
+            <div className="row_posters">
+                {/*several row_posters*/}
+
+                {movies.map(movie => (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                ))}
+            </div>
 
         </div>
     )
