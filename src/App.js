3757af850a4721ef765c9446b24abda9dba94824
jhed <gallegosmatiasjoel@gmail.com>
2021-01-26 13:54:44 -0300

    Deploy in Firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import Banner from './Banner';
 import requests from './requests';
 
 function App() {
   return (
     <div className="app">
-      <h1>Astrovideo clone frontend</h1>
+      
+      {/* Navbar*/}
+      <Banner />
+
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
 
---
+++ src/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <header>
+            {/*background image*/}
+            {/*title*/}
+            {/*div > 2 buttons*/}
+            {/*description*/}
+        </header>
+    )
+}
+
+export default Banner;
