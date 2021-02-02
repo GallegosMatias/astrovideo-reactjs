374a1fe277928bd64289d01a1ddd3a69177d9b2c
Irungaray <lionelirungaray@gmail.com>
2021-02-02 08:05:49 -0300

    Local images

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
+import netflix from "../images/netflix-logo.png";
+import avatar from "../images/avatar.jpg";
+
 import '../styles/Nav.css';
 
 function Nav() {
@@ -18,11 +21,8 @@ function Nav() {
 
     return (
         <div className={`nav ${show && "nav__black"}`}>
-
-            <img className="nav_logo" src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-4-1.png" alt=""/>
-            <img className="nav_avatar" src="https://i.pinimg.com/564x/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.jpg" alt=""/>
-
-            
+            <img src={netflix} className="nav_logo" alt="Netflix Logo"/>
+            <img src={avatar}  className="nav_avatar" alt=":)"/>
         </div>
     )
 }
